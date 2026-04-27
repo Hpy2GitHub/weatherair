405b5e813794c7eec35950d4c6d33a0f4a8867e1
MB <rho@f17tuf.localdomain>
2026-04-27 12:08:50 -0400

    cleanup

--- src/components/SunriseWidget.jsx
+++
@@ -1,9 +0,0 @@
-// src/components/SunriseWidget.jsx
-const export SunriseWidget = ({ data }) => {
-  return (
-    <div className="card">
-      <div className="section-label">Sun Times</div>
-     <p>My widget here</p> 
-    </div>
-  );
-};
